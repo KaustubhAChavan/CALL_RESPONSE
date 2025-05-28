14a22248edc1011f8494da115f88192afb2ac28e
KaustubhAChavan <108113091+KaustubhAChavan@users.noreply.github.com>
2025-05-28 20:26:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Call is successful and network is present"
+    # Create a TwiML response
+    twiml_response = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>Call is successful and network is present</Say>
+</Response>"""
+    return Response(twiml_response, mimetype='text/xml')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
